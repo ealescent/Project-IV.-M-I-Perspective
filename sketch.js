4575ec4c90825ef8dd0cf4ae5ccf5e103b201af0
StarfishSunset <72940354+StarfishSunset@users.noreply.github.com>
2020-11-30 13:26:10 +0800

    letterboxing

--- sketch.js
+++ sketch.js
@@ -1,12 +1,12 @@
 
 function setup() {
-  createCanvas(windowWidth, windowHeight);
+  createCanvas(1500, 800);
 }
 
 function draw() {
-  background(50);
+  background(23, 31, 38);
 
-  textSize(32);
-  fill(255);
-  text('Hello World', 20, 100);
+  fill(7, 10, 13)
+  rect(0, 0, width, 100)
+  rect(0, 700, width, 800)
 }

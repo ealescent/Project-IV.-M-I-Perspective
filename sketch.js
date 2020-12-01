795cb1673dad54dddb3db2f4bf82f7cea1907d8b
StarfishSunset <72940354+StarfishSunset@users.noreply.github.com>
2020-12-01 23:05:32 +0800

    idea of photos

--- sketch.js
+++ sketch.js
@@ -1,27 +1,28 @@
-// let slider;
+let slider;
 //j - the length of the background
-let j = 1500;
+// let j = 1500;
 
 function setup() {
-  createCanvas(1500, 800);
+  createCanvas(3000, 800);
 // slider = createSlider(0, 1500, 0);
 // slider.position(100, 700);
 // slider.style('width', '1300px');
 }
 
 function draw() {
-
   // let val = slider.value();
   background(23, 31, 38);
 
-for (x = 0; x < j; x++) {
-  fill(50);
-  rect(0, 0, x, 800);
+// for (x = 0; x < j; x++) {
+//   fill(50);
+//   rect(0, 0, x, 800);
+// }
+
+//photos
+for(p = 100; p < 3000; p+=700) {
+fill(50);
+noStroke();
+rect(p, 100, 600, 600);
 }
-  // push();
-  // fill(7, 10, 13);
-  // rect(0, 0, width, 100);
-  // rect(0, 700, width, 800);
-  // pop();
 
 }

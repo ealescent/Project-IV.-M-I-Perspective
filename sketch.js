1b67f86d6a6fd9ad61a759e2b73f2d3fd383e834
StarfishSunset <72940354+StarfishSunset@users.noreply.github.com>
2020-12-01 22:24:27 +0800

    clean up

--- sketch.js
+++ sketch.js
@@ -6,7 +6,10 @@ function setup() {
 function draw() {
   background(23, 31, 38);
 
-  fill(7, 10, 13)
-  rect(0, 0, width, 100)
-  rect(0, 700, width, 800)
+
+  push();
+  fill(7, 10, 13);
+  rect(0, 0, width, 100);
+  rect(0, 700, width, 800);
+  pop();
 }

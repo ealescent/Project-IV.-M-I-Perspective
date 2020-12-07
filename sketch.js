aaa807e72ca441bca122d5046e0784a454c4b613
StarfishSunset <72940354+StarfishSunset@users.noreply.github.com>
2020-12-07 09:30:43 +0800

    arrow clicker prototype

--- sketch.js
+++ sketch.js
@@ -1,16 +1,19 @@
 let slider;
 let bkg;
+let arrow;
 
 function setup() {
   createCanvas(3000, 800);
   bkg = loadImage("wave.jpg")
-// slider = createSlider(0, 1500, 0);
-// slider.position(100, 700);
-// slider.style('width', '1300px');
+slider = createSlider(0, 1500, 0);
+slider.position(100, 760);
+slider.style('width', '1300px');
+
+arrow = new Arrow();
 }
 
 function draw() {
-  // let val = slider.value();
+  let val = slider.value();
   background(23, 31, 38);
 
 //looping waves
@@ -24,6 +27,22 @@ for(p = 150; p < 3000; p+=700) {
 fill(50);
 noStroke();
 rect(p, 100, 600, 600);
+
+
+//arrow
+arrow.display();
+
 }
 
+class Arrow {
+  constructor() {
+    this.x = 700;
+    this.y = 400;
+    this.diameter = 30;
+  }
+
+  display() {
+    fill(255, 30);
+    circle(this.x, this.y, this.diameter);
+  }
 }

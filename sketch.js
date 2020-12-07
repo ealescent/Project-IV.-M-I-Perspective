be2dbfb72514a9bd99e5f541c4750aa7f142d7fc
StarfishSunset <72940354+StarfishSunset@users.noreply.github.com>
2020-12-07 11:08:53 +0800

    smth

--- sketch.js
+++ sketch.js
@@ -18,10 +18,7 @@ function draw() {
 
 //looping waves
 for(i = 50; i < 3000; i += 1136) {
-image(bkg, i = i - 1, 50, 1136, 700);
-// i+ frameCount (goes of the edge doesn't come back)
-// one more image than needed; instead of frameCount (move-pop)
-// frameCount % ...
+image(bkg, i, 50, 1136, 700);
 }
 
 //photos

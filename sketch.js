2d321c7152d8622d5430a1df82411749112697a9
StarfishSunset <72940354+StarfishSunset@users.noreply.github.com>
2020-12-07 10:57:18 +0800

    comments

--- sketch.js
+++ sketch.js
@@ -19,7 +19,9 @@ function draw() {
 //looping waves
 for(i = 50; i < 3000; i += 1136) {
 image(bkg, i = i - 1, 50, 1136, 700);
-
+// i+ frameCount (goes of the edge doesn't come back)
+// one more image than needed; instead of frameCount (move-pop)
+// frameCount % ...
 }
 
 //photos

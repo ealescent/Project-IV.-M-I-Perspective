259d34e5d0892b648e0705ac98bff3f435fb2b28
ealescent <eal506@nyu.edu>
2020-12-08 19:18:02 -0500

    progress

--- sketch.js
+++ sketch.js
@@ -1,24 +1,26 @@
 let slider;
 let bkg;
 let arrow;
+let easing = .05;
 
 function setup() {
-  createCanvas(3000, 800);
+  createCanvas(1000, 800);
   bkg = loadImage("wave.jpg")
 slider = createSlider(0, 1500, 0);
-slider.position(100, 760);
-slider.style('width', '1300px');
+slider.position(25, 760);
+slider.style('width', '950px');
 
 arrow = new Arrow();
 }
 
 function draw() {
   let val = slider.value();
-  //translate(-val, 0)
+  translate(-val*1.33, 0)
+  
   background(23, 31, 38);
 
 //looping waves
-for(i = 50; i < 100000; i += 1136) {
+for(i = 0; i < 100000; i += 1136) {
 image(bkg, i - frameCount/8, 50, 1136, 700);
 
 // i+ frameCount (goes off the edge doesn't come back)
@@ -26,7 +28,7 @@ image(bkg, i - frameCount/8, 50, 1136, 700);
 // frameCount % ...
 }
 //photos
-for(p = 150; p < 3000; p+=700) {
+for(p = 115; p < 3000; p+=725) {
 fill(50);
 noStroke();
 rect(p, 100, 600, 600);

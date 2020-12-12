552cd68827a7ea47c17af9621f5191b06b657095
StarfishSunset <72940354+StarfishSunset@users.noreply.github.com>
2020-12-12 11:06:57 +0800

    smth

--- sketch.js
+++ sketch.js
@@ -6,7 +6,6 @@ let lights;
 let song;
 let x = 40;
 let y = 11;
-
 function preload() {
   img = loadImage('vone.jpg');
   smile = loadImage('smile.JPG')
@@ -21,25 +20,20 @@ function setup() {
 slider = createSlider(0, 1500, 0);
 slider.position(25, 760);
 slider.style('width', '950px');
-
 }
-
 function draw() {
   let val = slider.value();
   translate(-val*1.38, 0)
   background(23, 31, 38);
-
 //looping waves
 for(i = 0; i < 100000; i += 1136) {
 image(bkg, i - frameCount/8, 50, 1136, 700);
 }
-
 //photos preview rects
 for(p = 800; p < 3000; p+=780) {
 fill(50);
 noStroke();
 rect(p, 100, 600, 600);
-
 //actual photos
 push();
 image(img, 150, 100, 3024/6.75, 4032/6.75);
@@ -47,10 +41,8 @@ image(smile, 810, 110, 580, 580)
 image(lights, 2370, 110, 580, 580)
 // console.log(img);
 pop();
-
 //PIXEL MANIPULATIONS
 //ETHAN SMILE MANIPULATION
-
 //slider info
 fill(50, 120);
 circle(50, 650, 70);
@@ -58,7 +50,6 @@ fill(255);
 text('Slider', 35, 645);
 text('below', 33, 660);
 }
-
 //song and song button
 push();
 fill(255, 100);
@@ -69,7 +60,6 @@ if (song.isPlaying()) {
 } else {
   text('Click to Play Music', 50, 30);
 }
-
 //info button 1
 push();
 fill(50, 230)
@@ -78,7 +68,6 @@ fill(255);
 textSize(16)
 text('i', 618, 115);
 pop()
-
   push();
   fill(50);
   rect(610, 130, 170, 60);
@@ -87,7 +76,6 @@ pop()
   text('Title: Shanghai in Blue', 610 + 10, 130 + 35);
   text('Time: Sep 2019', 610 + 10, 130 + 50);
   pop();
-
 //info button 2 (can't make a for loop as they are in different distances :( 
 push();
 fill(50, 230);
@@ -96,7 +84,6 @@ fill(255);
 textSize(16);
 text('i', 778, 574);
 pop();
-
   push()
   fill(50);
   rect(640, 590, 150, 78);
@@ -106,7 +93,6 @@ pop();
   text('Time: Summer 2019', 640 + 10, 590 + 50);
   text('Location: Cape Town', 640 + 10, 590 + 65);
   pop()
-
 //Info Button 3
 push();
 fill(50, 230)
@@ -115,7 +101,6 @@ fill(255);
 textSize(16)
 text('i', 1558, 115);
 pop()
-
   push();
   fill(50);
   rect(1420, 230, 150, 60);
@@ -124,7 +109,6 @@ pop()
   text('Title:', 1420 + 10, 230 + 35);
   text('Time:', 1420 + 10, 230 + 50);
   pop();
-
   
 //Info Button 4
   push();
@@ -145,7 +129,6 @@ pop()
     text('Location: Hangzhou', 2200 + 10, 530 + 65);
     pop();
  }
-
 //song
 function mousePressed() {
   var d = dist(mouseX, mouseY, x + 50, y - 20)
@@ -163,11 +146,9 @@ function mouseIsHovered() {
   return dist(mouseX, mouseY, 130, 110) < 35/2;
 }
 pop();
-
 //hover for info pic 2
 // push();
 // function mouseIsHovered() {
 //   return dist(mouseX, mouseY, 800, 110) < 35/2 
 // }
 // pop();
-

a7f3ce414bcd47c645398423701d6645c3e196f7
StarfishSunset <72940354+StarfishSunset@users.noreply.github.com>
2020-12-13 16:50:38 +0800

    setup done, scenes to do

--- sketch.js
+++ sketch.js
@@ -1,159 +1,157 @@
-let slider;
+let sceneIndex = 0;
 let bkg;
-let img;
-let smile;
+let shanghai;
+let simle;
+let sunset;
 let lights;
 let song;
-let sunset;
 let x = 40;
 let y = 11;
+
 function preload() {
-  img = loadImage('vone.jpg');
-  smile = loadImage('smile.JPG');
-  lights = loadImage('lights.JPG');
+  bkg = loadImage('wave.jpg');
+  shanghai = loadImage('vone.jpg');
+  smile = loadImage('smile.jpg');
   sunset = loadImage('pic2.jpg');
+  lights = loadImage('lights.jpg');
   song = loadSound('apocalypse.mp3');
-    song.setVolume(0.07)
+  song.setVolume(0.1);
 }
-  
+
 function setup() {
-  createCanvas(1000, 800);
-  bkg = loadImage("wave.jpg")
-slider = createSlider(0, 1500, 0);
-slider.position(25, 760);
-slider.style('width', '950px');
-}
-function draw() {
-  let val = slider.value();
-  translate(-val*1.38, 0)
+  createCanvas(3100, 800);
   background(23, 31, 38);
-//looping waves
-for(i = 0; i < 100000; i += 1136) {
-image(bkg, i - frameCount/8, 50, 1136, 700);
-}
-//photos preview rects
-for(p = 800; p < 3000; p+=780) {
-fill(50, 160);
-noStroke();
-rect(p, 100, 600, 600);
-//actual photos
-push();
-fill(50, 70)
-rect(140, 90, 468, 617)
-image(img, 150, 100, 3024/6.75, 4032/6.75);
-image(smile, 810, 110, 580, 580)
-image(lights, 2370, 110, 580, 580)
-image(sunset, 1590, 110, 580, 580)
-// console.log(img);
-pop();
-//PIXEL MANIPULATIONS
-//ETHAN SMILE MANIPULATION
-//slider info
-fill(50, 120);
-circle(50, 650, 70);
-fill(255);
-text('Slider', 35, 645);
-text('below', 33, 660);
-}
-//song and song button
-push();
+
+  //looping waves
+  for (i = 0; i < 3100; i+=1000) {
+    image(bkg, i, 50, 1000, 750);
+  }
+
+  //shadow frames
+  for(p = 800; p < 3000; p+=780) {
+    fill(50, 160);
+    noStroke();
+    rect(p, 100, 600, 600);
+  }
+  rect(140, 90, 468, 617);
+
+  //info buttons and boxes
+  fill(50, 240);
+  //1
+  circle(630, 110, 35);
+  rect(610, 130, 170, 60);
+  //2
+  circle(780, 570, 35);
+  rect(640, 590, 150, 78);
+  //3
+  circle(1560, 210, 35);
+  rect(1420, 230, 150, 60);
+  //4
+  circle(2340, 510, 35);
+  rect(2200, 530, 150, 75);
+  //text 'i' and 'data'
+  fill(255);
+  textSize(16);
+  text('i', 628, 115);
+  text('i', 778, 574);
+  text('i', 1558, 214);
+  text('i', 2338, 515);
+  textSize(12);
+  text('Author: Victoria Oury', 610 + 10, 130 + 20);
+  text('Title: 上海 in Blue', 610 + 10, 130 + 35);
+  text('Time: Sep 2019', 610 + 10, 130 + 50);
+  text('Author: Ethan Lan', 640 + 10, 590 + 20);
+  text('Title: "Smile!"', 640 + 10, 590 + 35);
+  text('Time: Summer 2019', 640 + 10, 590 + 50);
+  text('Location: Cape Town', 640 + 10, 590 + 65);
+  text('Author: Victoria Oury', 1420 + 10, 230 + 20);
+  text('Title: 杭州 in orange', 1420 + 10, 230 + 35);
+  text('Time: Winter break 2019', 1420 + 10, 230 + 50);
+  text('Author: Ethan Lan', 2200 + 10, 530 + 20);
+  text('Title: "Expletive Hallway"', 2200 + 10, 530 + 35);
+  text('Time: Fall 2019', 2200 + 10, 530 + 50);
+  text('Location: Hangzhou', 2200 + 10, 530 + 65);
+
+  //pic1
+image(shanghai, 150, 100, 3024/6.75, 4032/6.75);
+  //pic2
+image(smile, 810, 110, 580, 580);
+  //pic3
+image(sunset, 1590, 110, 580, 580);
+  //pic4
+image(lights, 2370, 110, 580, 580);
+
+//song
 fill(255, 100);
-rect(x, y, 120, 30, 10)
-pop();
+rect(x, y, 120, 30, 10);
+fill(255);
 if (song.isPlaying()) {
   text('Click to Stop Music', 50, 30);
 } else {
   text('Click to Play Music', 50, 30);
 }
-//info button 1
-push();
-fill(50, 230)
-circle(630, 110, 35);
-fill(255);
-textSize(16)
-text('i', 628, 115);
-pop()
-  push();
-  fill(50);
-  rect(610, 130, 170, 60);
-  fill(255);
-  text('Author: Victoria Oury', 610 + 10, 130 + 20);
-  text('Title: 上海 in Blue', 610 + 10, 130 + 35);
-  text('Time: Sep 2019', 610 + 10, 130 + 50);
-  pop();
-//info button 2 (can't make a for loop as they are in different distances :( 
-push();
-fill(50, 230);
-circle(780, 570, 35);
-fill(255);
-textSize(16);
-text('i', 778, 574);
-pop();
-  push()
-  fill(50);
-  rect(640, 590, 150, 78);
-  fill(255);
-  text('Author: Ethan Lan', 640 + 10, 590 + 20);
-  text('Title: "Smile!"', 640 + 10, 590 + 35);
-  text('Time: Summer 2019', 640 + 10, 590 + 50);
-  text('Location: Cape Town', 640 + 10, 590 + 65);
-  pop()
-//Info Button 3
-push();
-fill(50, 230)
-circle(1560, 210, 35);
-fill(255);
-textSize(16)
-text('i', 1558, 214);
-pop()
-  push();
-  fill(50);
-  rect(1420, 230, 150, 60);
-  fill(255);
-  text('Author: Victoria Oury', 1420 + 10, 230 + 20);
-  text('Title: 杭州 in orange', 1420 + 10, 230 + 35);
-  text('Time: Winter break 2019', 1420 + 10, 230 + 50);
-  pop();
-  
-//Info Button 4
-  push();
-  fill(50, 230)
-  circle(2340, 510, 35);
-  fill(255);
-  textSize(16)
-  text('i', 2338, 515);
-  pop()
-  
-    push();
-    fill(50);
-    rect(2200, 530, 150, 75);
-    fill(255);
-    text('Author: Ethan Lan', 2200 + 10, 530 + 20);
-    text('Title: "Expletive Hallway"', 2200 + 10, 530 + 35);
-    text('Time: Fall 2019', 2200 + 10, 530 + 50);
-    text('Location: Hangzhou', 2200 + 10, 530 + 65);
-    pop();
- }
-//song
+
 function mousePressed() {
   var d = dist(mouseX, mouseY, x + 50, y - 20)
-  if(d<80){
-    if (song.isPlaying()) {
-      song.pause();
-    } else {
-      song.play();
-    }
+  if (d < 80) {
+  if (song.isPlaying()) {
+    song.pause();
+  } else {
+    song.play();
+  }
   }
 }
-//hover for info pic 1
-push();
-function mouseIsHovered() {
-  return dist(mouseX, mouseY, 130, 110) < 35/2;
 }
-pop();
-//hover for info pic 2
-// push();
-// function mouseIsHovered() {
-//   return dist(mouseX, mouseY, 800, 110) < 35/2 
-// }
-// pop();
+
+function draw() {
+
+  if (sceneIndex === 0) {
+    drawIntro();
+  } else if (sceneIndex === 1) {
+    drawScene1();
+  } else {
+    drawEnding();
+  }
+
+  text("Click left or right of the center to change scenes.", width / 2, 50);
+}
+
+function keyPressed() {
+  if (keyCode === RIGHT_ARROW) {
+		previousScene();
+	} else {
+		nextScene();
+	}
+}
+
+function previousScene() {
+	sceneIndex--;
+	if (sceneIndex < 0) {
+		sceneIndex = 3;
+	}
+}
+
+function nextScene() {
+	sceneIndex = (sceneIndex + 1) % 5;
+}
+
+function drawIntro() {
+  fill(255);
+  ellipse(width / 2, height / 2, 100, 100);
+	fill("blue");
+	text("Intro", width / 2, height / 2);
+}
+
+function drawScene1() {
+  fill(0, 255, 255);
+  rect(width / 2, height / 2, 200, 200);
+	fill(0);
+	text("Scene 1", width / 2, height / 2);
+}
+
+function drawEnding() {
+  fill(255);
+  ellipse(width / 2, height / 2, 100, 100);
+	fill(0);
+	text("End Scene", width / 2, height / 2);
+}
